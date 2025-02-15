fc523bfe9b448487ae700bb0ca5e98e68393beb9
SEPILBOARD <sepilboard@gmail.com>
2025-02-15 22:34:26 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+using namespace std;
+
+int main()
+{
+    FASTIO
+    int n; cin >> n;
+    cout << 2000 <<"\n";
+    for(int i = 0; i<1000; i++) cout << "1 ";
+    for(int i = 0; i<1000; i++) cout << "1000 ";
+    return 0;
+}
