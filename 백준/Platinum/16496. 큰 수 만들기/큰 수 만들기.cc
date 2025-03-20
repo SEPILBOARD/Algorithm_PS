5f8ced75ca810b5b8a4eb01fa938676a48507368
SEPILBOARD <sepilboard@gmail.com>
2025-03-20 19:21:51 +0900

    [Platinum V] Title: 큰 수 만들기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.cc
+++ 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.cc
@@ -1,64 +1,28 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <math.h>
+#include <bits/stdc++.h>
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
 using namespace std;
 
 int n;
-unsigned long long a[1000];
-vector<int> r;
+bool flag = true;
 
-bool allZ = true;
-
-int main()
+bool compare(string f, string s)
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
+    return f+s>s+f;
+}
 
-    cin >> n;
-    for(int i =0; i<n; i++)
-    {
-        cin >> a[i];
-        if(a[i]) allZ = false;
+int main(){
+    cin>>n;
+    vector<string> arr(n);
+    for(auto &e : arr){
+        cin >> e;
+        if(e !="0") flag = false;
     }
-
-    if(allZ)
-    {
-        cout << 0;
+    if(flag){
+        cout << "0";
         return 0;
     }
+    sort(arr.begin(),arr.end(),compare);
+    for(auto e: arr) cout << e;
     
-    for(int i =0; i<n; i++)
-    {
-        int c =0;
-        while(c<i)
-        {
-            unsigned long long iLog = 1;
-            if(r[c]) iLog = log10(r[c])+1;
-            unsigned long long iFront = a[i] * pow(10, iLog) + r[c];
-
-            unsigned long long cLog = 1;
-            if(a[i]) cLog = log10(a[i])+1;
-            unsigned long long cFront = r[c] * pow(10, cLog) + a[i];
-
-            if(iFront > cFront)
-            {
-                r.insert(r.begin()+c, 1, a[i]);
-                break;
-            }
-            else c++;
-        }
-        if(c==i)
-        {
-            r.push_back(a[i]);
-        }
-    }
-
-    for(int k : r)
-    {
-        cout << k;
-    }
-
     return 0;
 }
