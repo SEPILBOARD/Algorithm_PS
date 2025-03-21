3ddfe92f1b8b0af194f9ae84206121bea5a374f8
SEPILBOARD <sepilboard@gmail.com>
2025-03-22 02:34:41 +0900

    [Gold V] Title: 가로등, Time: 336 ms, Memory: 35480 KB, Score: 25 point -BaekjoonHub

---
+++ 백준/Gold/32069. 가로등/가로등.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+using namespace std;
+#define int long long int
+#define MAX_K 500000
+
+int l, n, k;
+set<int> s;
+queue<pair<int, int>> q;
+
+void BFS()
+{
+    while(k--){
+        int dist = q.front().second;
+        cout << dist <<"\n";
+        int x = q.front().first;
+        s.insert(x);
+        q.pop();
+        if(!s.count(x-1) && x-1>=0) q.push({x-1, dist+1});
+        if(!s.count(x+1) && x+1<=l) q.push({x+1, dist+1});
+    }
+}
+
+signed main()
+{
+    FASTIO;
+    cin >> l >> n >> k;
+    for(int i = 0; i<n; i++){
+        int e; cin >> e;
+        q.push({e, 0});
+        s.insert(e);
+    }
+    BFS();
+    return 0;
+}
