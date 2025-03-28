0156e0d8b490040b6292e4ad04671e55139df13e
SEPILBOARD <sepilboard@gmail.com>
2025-03-29 01:59:14 +0900

    [Gold III] Title: 게임을 만들어요, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/32299. 게임을 만들어요/게임을 만들어요.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+using namespace std;
+
+int main()
+{
+    FASTIO;
+    int n; cin >> n;
+    if(n==3) cout << "Hobanwoo";
+    else cout << "Sangho";
+    return 0;
+}
