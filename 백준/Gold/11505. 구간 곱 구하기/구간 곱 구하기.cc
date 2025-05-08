69c07c17f13801947700af47796a297d67be84d7
SEPILBOARD <sepilboard@gmail.com>
2025-05-08 17:39:15 +0900

    [Gold I] Title: 구간 곱 구하기, Time: 136 ms, Memory: 41096 KB -BaekjoonHub

---
+++ 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.cc
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+using namespace std;
+#define int long long int
+#define MOD 1'000'000'007
+class SegmentTree
+{
+    private:
+        int n;
+        vector<long long> tree;
+
+        void build(const vector<int>& a, int idx, int start, int end)
+        {
+            if(start==end){
+                tree[idx] = a[end];
+            }
+            else{
+                int mid = (start+end)/2;
+                build(a, idx*2, start, mid); //왼쪽
+                build(a, idx*2+1, mid+1, end); //오른쪽
+
+                //연산
+                tree[idx] = (tree[2*idx] * tree[2*idx+1])%MOD;
+            }
+        }
+
+        void update(int idx, int start, int end, int point, int value)
+        {
+            if(start == end){
+                tree[idx] = value;
+                return;
+            }
+            else{
+                int mid = (start+end)/2;
+                if(point <= mid) update(idx*2, start, mid, point, value);
+                else update(idx*2+1, mid+1, end, point, value);
+
+                //연산
+                tree[idx] = (tree[2*idx] * tree[2*idx+1])%MOD;
+            }
+        }
+
+        long long query(int idx, int start, int end, int left, int right)
+        {
+            //범위 밖
+            if(left > end || right < start){
+                return 1;
+            }
+            //범위 완전 포함
+            if(left <= start && right >= end){
+                return tree[idx];
+            }
+            int mid = (start+end)/2;
+
+            return (query(2*idx, start, mid, left, right) * query(2*idx+1, mid+1, end, left, right))%MOD;
+        }
+
+    public:
+        SegmentTree(const vector<int>& a)
+        {
+            n = a.size();
+            tree.resize(4*n);
+            build(a, 1, 0, n-1);
+        }
+
+        void update(int idx, int value)
+        {   
+            update(1, 0, n-1, idx, value);
+        }
+
+        long long query(int left, int right)
+        {
+            return query(1, 0, n-1, left, right);
+        }      
+};
+
+signed main()
+{
+    FASTIO;
+    int n, m, k; cin >> n >> m >> k;
+    vector<int> v(n);
+    for(int i = 0; i<n; i++){
+        cin >> v[i];
+        v[i] %=MOD;
+    }
+    SegmentTree seg(v);
+
+    for(int i = 0; i<m+k; i++){
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        if(a==1) seg.update(b-1,c%MOD);
+        else cout << seg.query(b-1,c-1) <<"\n";
+    }
+    
+    return 0;
+}
