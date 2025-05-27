64eba7803eb05aa474e38e7d30cad43b7e75d103
SEPILBOARD <sepilboard@gmail.com>
2025-05-27 18:42:31 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+using namespace std;
+
+int n, m;
+int a[9];
+int ans[9];
+
+void bt(int idx, int cnt)
+{
+    if(cnt == m){
+        for(int i = 0; i<m; i++)
+            cout << ans[i] <<" ";
+        cout << "\n";
+        return;
+    }
+
+    for(int i = idx; i<n; i++){
+        ans[cnt]=a[i];
+        bt(i, cnt+1);
+    }
+}
+
+int main()
+{
+    FASTIO;
+    cin >> n >> m;
+    for(int i = 0; i<n; i++) cin >> a[i];
+    sort(a, a+n);
+    bt(0, 0);
+    return 0;
+}
