0efd1fb21cdcc6e05fd1a476eda84113f0993b31
SEPILBOARD <sepilboard@gmail.com>
2025-06-08 15:30:44 +0900

    [Silver I] Title: 킥다운, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1195. 킥다운/킥다운.cc
+++ 백준/Silver/1195. 킥다운/킥다운.cc
@@ -35,31 +35,6 @@ int main()
         }
         if(flag) ans = min(ans, n+m-cnt);
     }
-    // reverse(s.begin(), s.end());
-    for(int k = -n; k<m; k++){
-        int cnt = 0;
-        bool flag = true;
-        if(k<0){
-            for(int i = -k, j = 0; i<n && j<m; i++, j++){
-                cnt++;
-                if(s[i] == '2' && t[j] == '2'){
-                    flag = false;
-                    break;
-                }
-            }
-        }
-        else{
-            for(int i = 0, j = k; i<n && j<m; i++, j++){
-                cnt++;
-                if(s[i] == '2' && t[j] == '2'){
-                    flag = false;
-                    break;
-                }
-            }
-        }
-        if(flag) ans = min(ans, n+m-cnt);
-    }
-    // cout << m <<"\n";
     cout << ans;
     return 0;
 }
