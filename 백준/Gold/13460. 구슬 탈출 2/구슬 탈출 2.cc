756a027d9606dcfc90b65e372ec73f61e841618d
SEPILBOARD <sepilboard@gmail.com>
2025-06-10 19:29:41 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 36 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.cc
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+using namespace std;
+
+struct Point{
+    int x, y;
+    bool operator==(Point other){
+        return x == other.x && y == other.y;
+    }
+    bool operator!=(Point other){
+        return x != other.x || y != other.y;
+    }
+};
+
+int dx[4] = {0, 0, 1, -1};
+int dy[4] = {1, -1, 0, 0};
+
+int n, m;
+char graph[11][11];
+Point r, b, o;
+
+int ans = 999;
+
+void bt(int cnt)
+{
+    if(cnt==10) return;
+
+    int rx = r.x;
+    int ry = r.y;
+    int bx = b.x;
+    int by = b.y;
+
+    // cout << cnt <<"\n";
+    // for(int i = 0; i<n; i++){
+    //     for(int j = 0; j<m; j++){
+    //         cout << graph[i][j];
+    //     }
+    //     cout << "\n";
+    // }
+    // cout << "\n";
+
+    for(int i = 0; i<4; i++){
+        // cout << "i=" << i <<": " << b.y << " vs " << r.y <<"\n\n";
+
+        if((i==0 && b.y>r.y) || (i==1 && b.y<r.y) || (i==2 && b.x>r.x) || (i==3 && b.x<r.x)){
+            while(graph[b.x+dx[i]][b.y+dy[i]] == '.' && b!=o) b.x += dx[i], b.y += dy[i];
+            graph[bx][by] = '.';
+            graph[b.x][b.y] = 'B';
+            while(graph[r.x+dx[i]][r.y+dy[i]] == '.' && r!=o) r.x += dx[i], r.y += dy[i];
+            graph[rx][ry] = '.';
+            graph[r.x][r.y] = 'R';
+        }
+        else{
+            while(graph[r.x+dx[i]][r.y+dy[i]] == '.' && r!=o) r.x += dx[i], r.y += dy[i];
+            graph[rx][ry] = '.';
+            if(r==o) graph[r.x][r.y] = '.';
+            else graph[r.x][r.y] = 'R';
+            while(graph[b.x+dx[i]][b.y+dy[i]] == '.' && b!=o) b.x += dx[i], b.y += dy[i];
+            graph[bx][by] = '.';
+            graph[b.x][b.y] = 'B';
+        }
+        
+        if(b==o);
+        else if(r==o) ans = min(ans, cnt+1);
+        else bt(cnt+1);
+
+        graph[r.x][r.y] = '.';
+        graph[rx][ry] = 'R';
+        graph[b.x][b.y] = '.';
+        graph[bx][by] = 'B';
+        r.x = rx;
+        r.y = ry;
+        b.x = bx;
+        b.y = by;
+        // else{
+        //     cout <<cnt <<": " <<"i=" <<i <<": x\n";
+        // }
+    }
+}
+
+int main()
+{
+    FASTIO;
+    cin >> n >> m;
+
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<m; j++){
+            cin >> graph[i][j];
+            if(graph[i][j] == 'B') b.x = i, b.y = j;
+            else if(graph[i][j] == 'R') r.x = i, r.y = j;
+            else if(graph[i][j] == 'O'){
+                graph[i][j] = '.';
+                o.x = i, o.y = j;
+            }
+        }
+    }
+    bt(0);
+    if(ans == 999) cout << -1;
+    else cout << ans;
+    return 0;
+}
