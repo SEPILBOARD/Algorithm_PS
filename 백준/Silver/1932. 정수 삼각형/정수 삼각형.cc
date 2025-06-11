71a014413d3a2238b55c3b3848265df5ec0ab5cb
SEPILBOARD <sepilboard@gmail.com>
2025-06-12 03:14:02 +0900

    [Silver I] Title: 정수 삼각형, Time: 8 ms, Memory: 3000 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -3,28 +3,20 @@
 using namespace std;
 
 int n;
-int tgl[501][501];
-
 int dp[501][501];
 
 int main()
 {
     FASTIO;
     cin >> n;
-    for(int i = 1; i<=n; i++){
-        for(int j = 1; j<=i; j++){
-            cin >> tgl[i][j];
-        }
-    }
-    
-    for(int i = 1; i<=n; i++) dp[n][i] = tgl[n][i];
+    for(int i = 1; i<=n; i++)
+        for(int j = 1; j<=i; j++)
+            cin >> dp[i][j];
 
-    for(int i = n-1; i>=1; i--){
-        for(int j = 1; j<=i; j++){
-            dp[i][j] = tgl[i][j] + dp[i+1][j];
-            dp[i][j] = max(dp[i][j], tgl[i][j] + dp[i+1][j+1]);
-        }
-    }
+    for(int i = n-1; i>=1; i--)
+        for(int j = 1; j<=i; j++)
+            dp[i][j] += max(dp[i+1][j], dp[i+1][j+1]);
+            
     cout << dp[1][1];
     return 0;
 }
