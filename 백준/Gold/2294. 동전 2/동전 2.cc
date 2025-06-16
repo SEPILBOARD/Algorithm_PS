659e115a71426e778aa78a7d6e8901626770b2f2
SEPILBOARD <sepilboard@gmail.com>
2025-06-16 20:27:55 +0900

    [Gold V] Title: 동전 2, Time: 4 ms, Memory: 6064 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.cc
+++ 백준/Gold/2294. 동전 2/동전 2.cc
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 #define FASTIO ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
 using namespace std;
+#define INF 2139062143
 
 int n, k;
 bool v[100'001];
@@ -12,25 +13,24 @@ int main()
 {
     FASTIO;
     cin >> n >> k;
+    c.push_back(-1);
     for(int i = 0; i<n; i++){
         int e; cin >> e;
         if(v[e]) continue;
         v[e] = true;
         c.push_back(e);
     }
-    memset(dp, -1, sizeof(dp));
+    
+    memset(dp, 0x7f, sizeof(dp));
     dp[0][0] = 0;
-    n = c.size();
-    for(int i = 0; i<n; i++){
+    n = c.size()-1;
+    for(int i = 1; i<=n; i++){
         for(int j = 0; j<=k; j++){
-            if(dp[i][j] == -1) continue;
-            for(int cnt = 0; cnt*c[i]+j<=k; cnt++){
-                if(dp[i+1][cnt*c[i]+j] == -1) dp[i+1][cnt*c[i]+j] = dp[i][j] + cnt;
-                else dp[i+1][cnt*c[i]+j] = min(dp[i+1][cnt*c[i]+j], dp[i][j] + cnt);
-            }
+            dp[i][j] = dp[i-1][j];
+            if(j-c[i]>=0) dp[i][j] = min(dp[i][j], dp[i][j-c[i]]+1);
         }
     }
 
-    cout << dp[n][k];
+    cout << (dp[n][k]==INF ?-1:dp[n][k]);
     return 0;
 }
