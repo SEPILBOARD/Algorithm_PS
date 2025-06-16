a2c8ed1bbde3e5ac8eb3e6f6104da87be5c7bd11
SEPILBOARD <sepilboard@gmail.com>
2025-06-16 20:42:23 +0900

    [Gold V] Title: 동전 2, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.cc
+++ 백준/Gold/2294. 동전 2/동전 2.cc
@@ -6,8 +6,7 @@ using namespace std;
 int n, k;
 bool v[100'001];
 vector<int> c;
-
-int dp[101][10'001];
+int dp[10'001];
 
 int main()
 {
@@ -20,17 +19,16 @@ int main()
         v[e] = true;
         c.push_back(e);
     }
-    
+
     memset(dp, 0x7f, sizeof(dp));
-    dp[0][0] = 0;
+    dp[0] = 0;
     n = c.size()-1;
     for(int i = 1; i<=n; i++){
-        for(int j = 0; j<=k; j++){
-            dp[i][j] = dp[i-1][j];
-            if(j-c[i]>=0) dp[i][j] = min(dp[i][j], dp[i][j-c[i]]+1);
+        for(int j = 0; j+c[i]<=k; j++){
+            dp[j+c[i]] = min(dp[j+c[i]], dp[j]+1);
         }
     }
 
-    cout << (dp[n][k]==INF ?-1:dp[n][k]);
+    cout << (dp[k]==INF ?-1:dp[k]);
     return 0;
 }
