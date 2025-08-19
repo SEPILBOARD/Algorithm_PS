55f53f409519eb27d9073cbac1716d7c05617b06
SEPILBOARD <sepilboard@gmail.com>
2025-08-20 01:42:39 +0900

    [Gold II] Title: 대운하, Time: 32 ms, Memory: 8428 KB -BaekjoonHub

---
+++ 백준/Gold/2350. 대운하/대운하.cc
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+using namespace std;
+
+int n, m, k;
+int p[1'001];
+vector<pair<int, pair<int, int>>> edge;
+vector<int> cc[1'001];
+int ans[1'001][1'001];
+
+int Find(int x)
+{
+    if(x == p[x]) return x;
+    else return p[x] = Find(p[x]);
+}
+
+void Union(int x, int y, int w)
+{
+    x = Find(x);
+    y = Find(y);
+    if(x==y) return;
+
+    if(cc[x].size() < cc[y].size()){
+        for(int xc: cc[x]){
+            p[x] = y;
+            cc[y].push_back(xc);
+        }
+    }
+    else{
+        p[y] = x;
+        for(int yc: cc[y]){
+            cc[x].push_back(yc);
+        }
+    }
+
+    for(int xc: cc[x]){
+        for(int yc: cc[y]){
+            ans[xc][yc] = max(w, ans[xc][yc]);
+            ans[yc][xc] = max(w, ans[yc][xc]);
+        }
+    }
+}
+
+bool compare(pair<int, pair<int, int>> fs, pair<int, pair<int,int>> sc)
+{
+    return fs.first > sc.first;
+}
+
+signed main()
+{
+    FASTIO;
+    // memset(ans, 0x7f, sizeof(ans));
+    cin >> n >> m >> k;
+    for(int i = 1; i<=n; i++){
+        p[i] = i;
+        cc[i].push_back(i);
+    }
+
+    for(int i = 0; i<m; i++){
+        int s, e, w;
+        cin >> s >> e >> w;
+        edge.push_back({w, {s,e}});
+    }
+    sort(edge.begin(), edge.end(), compare);
+
+    for(auto[w, v]: edge){
+        auto[s, e] = v;
+        Union(s, e, w);
+    }
+
+    for(int i = 0; i<k; i++){
+        int s, e;
+        cin >> s >> e;
+        cout << ans[s][e] <<"\n";
+    }
+    return 0;
+}
