fb229f4d2f1ac5d4f19cd29e9fcc55560f9b7d03
SEPILBOARD <sepilboard@gmail.com>
2025-09-12 00:18:42 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 196 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+using namespace std;
+
+int a, b;
+
+void solve()
+{
+    cin >> a >> b;
+    cout << a+b <<"\n";
+}
+
+signed main()
+{
+    FASTIO;
+    int _tc; cin >> _tc;
+    while(_tc--) solve();
+    return 0;
+}
