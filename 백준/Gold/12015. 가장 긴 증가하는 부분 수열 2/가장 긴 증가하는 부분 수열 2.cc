c970b3dfe066cd26d96b23d1cf9201c5e5e11f3a
SEPILBOARD <sepilboard@gmail.com>
2025-09-12 23:32:28 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 140 ms, Memory: 12200 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+using namespace std;
+
+int n;
+int a[1'000'001];
+vector<int> l;
+
+int main()
+{
+    FASTIO;
+    cin >> n;
+    for(int i = 0; i<n; i++) cin >> a[i];
+
+    l.push_back(-1);
+    for(int i = 0; i<n; i++){
+        if(a[i]>l.back()){
+            l.push_back(a[i]);
+        }
+        else{
+            int idx = lower_bound(l.begin(), l.end(), a[i]) - l.begin();
+            l[idx] = a[i];
+        }
+    }
+    cout << l.size()-1;
+    return 0;
+}
