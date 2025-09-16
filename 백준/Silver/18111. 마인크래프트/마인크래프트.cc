85e7db92fc524e3ff38e54d6f44cdf2f77597e7f
SEPILBOARD <sepilboard@gmail.com>
2025-09-17 01:17:16 +0900

    [Silver II] Title: 마인크래프트, Time: 144 ms, Memory: 3980 KB -BaekjoonHub

---
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+using namespace std;
+#define int long long
+
+int n, m, b;
+int a[501][501];
+
+signed main()
+{
+    FASTIO;
+    cin >> n >> m >> b;
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<m; j++){
+            cin >> a[i][j];
+        }
+    }
+
+    int anst = 1e+9;
+    int ansh = 0;
+    for(int h = 0; h<=b/(m*n)+260; h++){
+        int curb = b;
+        int t = 0;
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(a[i][j] > h){
+                    t += 2*(a[i][j]-h);
+                    curb += a[i][j]-h;
+                }
+                else if(a[i][j] < h){
+                    t += h-a[i][j];
+                    curb -= h-a[i][j];
+                }
+            }
+        }
+
+        if(curb < 0) continue;
+
+        if(anst >= t){
+            anst = t;
+            ansh = h;
+        }
+    }
+    cout << anst << " " << ansh;
+    return 0;
+}
