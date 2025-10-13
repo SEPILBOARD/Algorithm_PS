b54e473e23bc883cf8d72b2e9404872a2ef07a0b
SEPILBOARD <sepilboard@gmail.com>
2025-10-13 22:02:02 +0900

    [Silver I] Title: 신입 사원, Time: 480 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+using namespace std;
+typedef long long int ll;
+
+int n;
+pair<int, int> a[100'001];
+
+bool compare(pair<int, int> &fs, pair<int, int> &sc)
+{
+    if(fs.first == sc.first) return fs.second > sc.second;
+    return fs.first < sc.first;
+}
+
+void solve()
+{
+    cin >> n;
+    for(int i = 0; i<n; i++) cin >> a[i].first >> a[i].second;
+
+    sort(a, a+n, compare);
+
+    int cur = a[0].second;
+    int ans = 1;
+    for(int i = 1; i<n; i++){
+        if(cur > a[i].second){
+            cur = a[i].second;
+            ans++;
+        }
+    }
+
+    // for(int i = 0; i<n; i++){
+    //     cout << a[i].first << " " << a[i].second <<"\n";
+    // }
+    
+    cout << ans <<"\n";
+}
+
+signed main()
+{
+    FASTIO;
+    int _tc; cin >> _tc;
+    while(_tc--) solve();
+    return 0;
+}
