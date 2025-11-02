f9b357b7ca7cd437c1366d0cf14202683d4e0735
SEPILBOARD <sepilboard@gmail.com>
2025-11-02 14:38:27 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 136 ms, Memory: 20860 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+using namespace std;
+typedef long long int ll;
+
+int n, m;
+map<string, int> idx;
+string s[100'001];
+
+signed main()
+{
+    FASTIO;
+    cin >> n >> m;
+    for(int i = 1; i<=n; i++){
+        cin >> s[i];
+        idx[s[i]] = i;
+    }
+
+    for(int i = 0; i<m; i++){
+        string input;
+        cin >> input;
+        if('1'<= input[0] && input[0]<='9'){
+            cout << s[stoi(input)] <<"\n";
+        }
+        else{
+            cout << idx[input] <<"\n";
+        }
+    }
+
+    return 0;
+}
