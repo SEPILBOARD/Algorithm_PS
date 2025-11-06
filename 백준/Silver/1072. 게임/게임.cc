95dd8e53f8a5ed8142d101381765d1a5cb5ea172
SEPILBOARD <sepilboard@gmail.com>
2025-11-07 01:44:32 +0900

    [Silver III] Title: 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1072. 게임/게임.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+using namespace std;
+typedef long long int ll;
+
+ll x, y, z;
+
+signed main()
+{
+    FASTIO;
+    cin >> x >> y;
+    
+    z = 100LL*y/x;
+    if(z>=99){
+        cout << "-1";
+        return 0;
+    }
+    ll lo = 0, hi = 1e+18;
+    while(lo+1<hi){
+        ll mid = lo+hi>>1;
+        if(z != 100LL*(y+mid)/(x+mid)){
+            hi = mid;
+        }
+        else{
+            lo = mid;
+        }
+    }
+    cout << hi;
+    return 0;
+}
