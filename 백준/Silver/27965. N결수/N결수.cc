a2f5c4f53f11ffe22a5f4b9b78b9d0f733d08ca6
SEPILBOARD <sepilboard@gmail.com>
2025-11-25 21:00:51 +0900

    [Silver III] Title: N결수, Time: 124 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/27965. N결수/N결수.cc
+++ 백준/Silver/27965. N결수/N결수.cc
@@ -4,22 +4,19 @@ using namespace std;
 typedef long long int ll;
 
 int n, k;
-int pw[10];
 
 signed main()
 {
     FASTIO;
     cin >> n >> k;
 
-    pw[0] = 1;
-    for(int i = 0; i<9; i++){
-        pw[i+1] = pw[i]*10;
-    }
-
+    int t = 10;
     ll ans = 0;
     for(int i = 1; i<=n; i++){
-        ans = (ans*pw[int(log10(i)+1)]+i)%k;
+        if(t<=i)t*=10;
+        ans = (ans*t+i)%k;
     }
+
     cout << ans;
     return 0;
 }
