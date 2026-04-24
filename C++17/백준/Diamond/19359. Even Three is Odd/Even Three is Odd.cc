74e4a7a33e4560807fb7039143b398be3eb5672e
SEPILBOARD <sepilboard@gmail.com>
2026-04-24 16:52:48 +0900

    [Diamond IV] Title: Even Three is Odd, Time: 148 ms, Memory: 2364 KB -BaekjoonHub

--- C++17/백준/Diamond/19359. Even Three is Odd/Even Three is Odd.cc
+++ C++17/백준/Diamond/19359. Even Three is Odd/Even Three is Odd.cc
@@ -6,31 +6,28 @@ using namespace std;
 #define int long long int
 
 int w[2001];
+
 signed main()
 {
     FASTIO
     int n;
     while(cin >> n){
-        // 0: 뒤에서 3번째, 1: 뒤에서 2번째, 2: 맨뒤
         vector<vector<int>> dp_p(3, vector<int>(n, 0));
         vector<vector<int>> dp_c(3, vector<int>(n, 0));
         for(int i = 0; i < n; i++) cin >> w[i];
 
         for(int i = 0; i < n; i++){
             for(int p = 0; p<3; p++){
-                dp_p[0][i] = w[i];
-                dp_p[1][i] = ((i+1) * w[i])%MOD;
-                dp_p[2][i] = ((i+1)*(i+1) * w[i])%MOD;
+                dp_p[p][i] = (w[i] * (int)pow(i+1, p)) % MOD;
             }
         }
 
         for(int idx = 3; idx < n; idx++){
+            for(int p = 0; p<3; p++) dp_c[p][0] = 0;
             dp_c[2][0] = dp_p[2][0];
             for(int i = 1; i < n; i++){
                 dp_c[2][i] = dp_c[2][i-1];
-                dp_c[2][i] = (dp_c[2][i]+ i*i*dp_p[0][i])%MOD;
-                dp_c[2][i] = (dp_c[2][i]+ i*dp_p[1][i])%MOD;
-                dp_c[2][i] = (dp_c[2][i]+ dp_p[2][i])%MOD;          
+                for(int p = 0; p<3; p++) dp_c[2][i] = (dp_c[2][i]+ (int)pow(i, 2-p)*dp_p[p][i])%MOD;             
             }
 
             int temp = 0;
